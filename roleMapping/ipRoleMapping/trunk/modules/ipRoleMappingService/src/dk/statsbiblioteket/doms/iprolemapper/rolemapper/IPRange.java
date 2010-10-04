d1cd9411e1d927e3096efd0fa76ce621739a7e44
thomassh <thomassh@31bcc958-5192-48e2-8e17-0a6350180d47>
2010-10-04 14:31:35 +0000

    Checking unfinished and non-working code for a IP address to role mapping container.

---
+++ roleMapping/ipRoleMapping/trunk/modules/ipRoleMappingService/src/dk/statsbiblioteket/doms/iprolemapper/rolemapper/IPRange.java
@@ -0,0 +1,89 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The DOMS project.
+ * Copyright (C) 2007-2010  The State and University Library
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package dk.statsbiblioteket.doms.iprolemapper.rolemapper;
+
+import java.net.InetAddress;
+import java.util.List;
+
+/**
+ *@author &lt;tsh@statsbiblioteket.dk&gt; Thomas Skou Hansen
+ */
+class IPRange {
+
+    private final InetAddress beginAddress;
+    private final InetAddress endAddress;
+    private final List<String> roles;
+
+    /**
+     * Create an IP range starting at <code>beginAddress</code> and ending at
+     * <code>endAddress</code>. Both addresses are included in the range. <\p>
+     * The IP range is associated with the roles specified by <code>roles</code>
+     * 
+     * @param beginAddress
+     *            the start address of the range.
+     * @param endAddress
+     *            the end address of the range.
+     * @param roles
+     *            the roles associated with the range.
+     * @throws IllegalArgumentException
+     *             if the begin address and end address is not of the same type.
+     *             I.e. if they are not both IPv4 of IPv6 addresses.
+     */
+    IPRange(InetAddress beginAddress, InetAddress endAddress, List<String> roles)
+            throws IllegalArgumentException {
+        if (beginAddress.getClass() != endAddress.getClass()) {
+            throw new IllegalArgumentException("The begin and end addresses "
+                    + "must be of the same type. beginAddress.getClas() = "
+                    + beginAddress.getClass() + "  endAddress.getClass() = "
+                    + endAddress.getClass());
+        }
+        this.beginAddress = beginAddress;
+        this.endAddress = endAddress;
+        this.roles = roles;
+    }
+
+    /**
+     * @return the begin address of this IP range.
+     */
+    InetAddress getBeginAddress() {
+        return beginAddress;
+    }
+
+    /**
+     * @return the end address of this IP range.
+     */
+    InetAddress getEndAddress() {
+        return endAddress;
+    }
+
+    /**
+     * @return the roles associated with this IP range.
+     */
+    public List<String> getRoles() {
+        return roles;
+    }
+}
---
+++ roleMapping/ipRoleMapping/trunk/modules/ipRoleMappingService/src/dk/statsbiblioteket/doms/iprolemapper/rolemapper/InetAddressComparator.java
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The DOMS project.
+ * Copyright (C) 2007-2010  The State and University Library
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package dk.statsbiblioteket.doms.iprolemapper.rolemapper;
+
+import java.net.InetAddress;
+import java.util.Comparator;
+
+/**
+ *@author &lt;tsh@statsbiblioteket.dk&gt; Thomas Skou Hansen
+ */
+public class InetAddressComparator implements Comparator<InetAddress> {
+
+    /* (non-Javadoc)
+     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+     */
+    public int compare(InetAddress inetAddress1, InetAddress inetAddress2) {
+        if (inetAddress1.getClass() != inetAddress2.getClass()) {
+            throw new ClassCastException("The InetAddress instances to "
+                    + "compare are not of the same sub-type. "
+                    + "inetAddress1.getClass() = " + inetAddress1.getClass()
+                    + "  inetAddress2.getClass() = " + inetAddress2.getClass());
+        }
+        
+        int returnVal = 0;
+        final byte inetAddress2bytes[] = inetAddress2.getAddress();
+        final byte inetAddress1bytes[] = inetAddress1.getAddress();
+        
+        // This should already be ensured by the verification of the class
+        // types above.
+        assert(inetAddress1bytes.length == inetAddress2bytes.length);
+        
+        for (int index = 0; index < inetAddress1bytes.length; index++) {
+            if (inetAddress1bytes[index] == inetAddress2bytes[index]) {
+                // It's all good, just keep the 0 return value.
+                continue;
+            }
+            
+            if (inetAddress1bytes[index] < inetAddress2bytes[index]) {
+                returnVal = -1;
+            } else {
+                returnVal = 1;
+            }
+            break;
+        }
+        return returnVal;
+    }
+}
